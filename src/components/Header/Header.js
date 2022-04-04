e8fd033a92e2fe659bd4bd0a8995a491f3476c9b
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-05 03:33:20 +0600

    change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className='sticky top-0 bg-white h-20'>
       <div className='flex justify-between items-center h-full max-w-7xl mx-auto p-8'>
-        <h1 className='font-bold text-3xl'>My Watch</h1>
+        <Link to={'/'}>
+          <h1 className='font-bold text-3xl'>My Watch</h1>
+        </Link>
         <nav className='flex gap-5 font-semibold text-xl'>
           <Link className='hover:text-primary' to={'/'}>
             Home
