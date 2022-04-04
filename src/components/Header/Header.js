72950fb9efc7851038e2d434ae7a70c04ddee1de
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-05 03:11:57 +0600

    change header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,25 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center sticky top-0 bg-white h-20 max-w-7xl mx-auto p-8'>
-      <h1 className='font-bold text-3xl'>My Watch</h1>
-      <nav className='flex gap-5 font-semibold text-xl'>
-        <Link className='hover:text-primary' to={'/'}>
-          Home
-        </Link>
-        <Link className='hover:text-primary' to={'/reviews'}>
-          Reviews
-        </Link>
-        <Link className='hover:text-primary' to={'/dashboard'}>
-          Dashboard
-        </Link>
-        <Link className='hover:text-primary' to={'/blogs'}>
-          Blogs
-        </Link>
-        <Link className='hover:text-primary' to={'/about'}>
-          About
-        </Link>
-      </nav>
+    <header className='sticky top-0 bg-white h-20'>
+      <div className='flex justify-between items-center h-full max-w-7xl mx-auto p-8'>
+        <h1 className='font-bold text-3xl'>My Watch</h1>
+        <nav className='flex gap-5 font-semibold text-xl'>
+          <Link className='hover:text-primary' to={'/'}>
+            Home
+          </Link>
+          <Link className='hover:text-primary' to={'/reviews'}>
+            Reviews
+          </Link>
+          <Link className='hover:text-primary' to={'/dashboard'}>
+            Dashboard
+          </Link>
+          <Link className='hover:text-primary' to={'/blogs'}>
+            Blogs
+          </Link>
+          <Link className='hover:text-primary' to={'/about'}>
+            About
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
