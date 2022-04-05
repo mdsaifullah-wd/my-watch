09c286a594df3007ca5c140e9ab37286fcfbf7b5
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-05 15:49:58 +0600

    update banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
   return (
     <section className='grid md:grid-cols-2 gap-10 items-center max-w-7xl mx-auto p-8 h-[calc(100vh-80px)]'>
-      <div className='font-bold text-6xl'>
+      <div className='font-bold text-5xl'>
         <h2 className='mb-2'>The Home for the </h2>
         <h2 className='text-primary mb-10'>Greatest Collections</h2>
         <p className='text-xl text-zinc-500'>
@@ -16,6 +16,7 @@ const Banner = () => {
         <img
           src='https://img.freepik.com/free-photo/elegant-watch-with-silver-golden-chain-isolated_181624-27080.jpg?t=st=1649006176~exp=1649006776~hmac=5c5ecaa914f5d24e7396ba2d225116a4afc826a990bea648195e90e1754e4f7a&w=826'
           alt='Watch'
+          className='w-3/4 ml-auto'
         />
       </div>
     </section>
