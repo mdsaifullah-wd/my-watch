3d7940bcd8ba77591a1112737ce8d3ec4ae808c3
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-05 21:25:53 +0600

    update footer color and text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-gray py-10 bottom-0'>
+    <footer className='bg-slate-800 py-10 bottom-0'>
       <p className='text-white text-center text-lg'>
-        © 202 MyWatch. All Rights Reserved
+        © 2022 MyWatch. All Rights Reserved
       </p>
     </footer>
   );
